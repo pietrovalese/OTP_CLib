32c742d37a4e1adda8a71cba91fc4e8a8af379cd
Pietro <p.valese@studenti.unipi.it>
2025-03-13 17:34:59 +0100

    BBS upgrade

--- include/bbs.h
+++ include/bbs.h
@@ -1,16 +1,37 @@
 #ifndef BBS_H
 #define BBS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <stdbool.h>
 
-// Genera due numeri primi p e q congrui a 3 modulo 4 e li memorizza in res[0] e res[1]
+/**
+ * Inizializza il generatore di numeri casuali.
+ * Da chiamare prima di usare altre funzioni di BBS.
+ */
+void bbs_init(void);
+
+/**
+ * Genera due numeri primi p e q congrui a 3 modulo 4.
+ * I valori vengono memorizzati in res[0] e res[1].
+ *
+ * @param res Array di almeno due elementi per memorizzare p e q.
+ */
 void gen_pq_BBS(long long *res);
 
-// Genera una sequenza di byte casuali usando l'algoritmo Blum Blum Shub (BBS)
+/**
+ * Genera una sequenza di byte casuali usando l'algoritmo Blum Blum Shub (BBS).
+ *
+ * @param buffer Array in cui verranno memorizzati i byte casuali.
+ * @param length Numero di byte da generare.
+ */
 void bbs_generate_bytes(uint8_t *buffer, int length);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BBS_H
--- src/bbs.c
+++ src/bbs.c
@@ -3,13 +3,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <math.h>
-#include "Miller_Rabin_PT.h"
+#include <time.h>
+#include <sys/random.h>  
+#include "Miller_Rabin_PT.h" 
 #include "bbs.h"
 
-
-static const long long range_start = 1000000;
-static const long long range_end = 100000000;
-
+#define RANGE_START 1000000
+#define RANGE_END 100000000
 
 // Funzione per calcolare il Massimo Comun Divisore (MCD)
 static long long mcd(long long a, long long b) {
@@ -21,48 +21,61 @@ static long long mcd(long long a, long long b) {
     return a;
 }
 
-// Funzione per generare due numeri primi congrui a 3 modulo 4
-void gen_pq_BBS(long long *res) {
-    srand(time(NULL));
+uint64_t get_secure_random() {
+    uint64_t r;
+    if (getrandom(&r, sizeof(r), 0) != sizeof(r)) {
+        perror("Errore in getrandom()");
+        exit(EXIT_FAILURE);
+    }
+    return r;
+}
 
-    long long p, q;
+// Genera un numero primo congruo a 3 modulo 4
+long long generate_prime_mod4_3(long long start, long long end) {
+    long long p;
     do {
-        p = range_start + (rand() % (range_end - range_start + 1));
+        p = start + (get_secure_random() % (end - start + 1));
         if (p % 2 == 0) p++;  // Rendi p dispari
         while (!(p % 4 == 3 && Test_Miller_Rabin(p))) {
             p += 2;
         }
+    } while (p < start || p > end);
+    return p;
+}
 
-        q = range_start + (rand() % (range_end - range_start + 1));
-        if (q % 2 == 0) q++;  // Rendi q dispari
-        while (!(q % 4 == 3 && Test_Miller_Rabin(q))) {
-            q += 2;
-        }
-    } while (p == q); // Assicura che p ≠ q
+// Genera due numeri primi p e q congrui a 3 modulo 4
+void gen_pq_BBS(long long *res) {
+    long long p = generate_prime_mod4_3(RANGE_START, RANGE_END);
+    long long q;
+    do {
+        q = generate_prime_mod4_3(RANGE_START, RANGE_END);
+    } while (p == q);  // Assicura che p ≠ q
 
     res[0] = p;
     res[1] = q;
 }
 
-// Funzione per generare byte casuali con Blum Blum Shub
+// Genera un valore iniziale y tale che MCD(y, num) == 1
+long long generate_secure_y(long long num) {
+    long long y = get_secure_random() % (RANGE_END - RANGE_START) + RANGE_START;
+    while (mcd(y, num) != 1) {
+        y++;
+    }
+    return y;
+}
+
+// Genera una sequenza di byte casuali con Blum Blum Shub
 void bbs_generate_bytes(uint8_t *buffer, int length) {
     long long pq[2];
     gen_pq_BBS(pq);
     long long p = pq[0], q = pq[1];
     long long num = p * q;
 
-    srand(time(NULL));
-    long long y = range_start + (rand() % (range_end - range_start + 1));
-
-    // Trova y tale che MCD(y, num) == 1
-    while (mcd(y, num) != 1) {
-        y++;
-    }
-
+    long long y = generate_secure_y(num);
     long long x = (y * y) % num;
+
     for (int i = 0; i < length; i++) {
-        buffer[i] = (uint8_t)(x & 0xFF); // Prendi solo gli ultimi 8 bit
+        buffer[i] = (uint8_t)(x & 0xFF);  // Prendi gli ultimi 8 bit
         x = (x * x) % num;
     }
 }
-
